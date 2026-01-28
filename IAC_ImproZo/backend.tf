915a9b79e6dffea1b63e6a5ddd65a60cc97adaa0
Dixit Rathi <121105687+dixitrathi7@users.noreply.github.com>
2026-01-28 15:27:32 +0530

    Update backend.tf

--- IAC_ImproZo/backend.tf
+++ IAC_ImproZo/backend.tf
@@ -3,7 +3,8 @@ terraform {
     storage_account_name = "rfqinfrastatefiles"
     container_name = "improzo-tofu-state-files"
     key = "improzo.tfstate"
-    access_key = "3JWPHZUc67k3JR88OAjUgl6xwiRfXUYibE6ckhh8zQIl3ZD4gn+M279qaR3kWyZY1zzFwnr1NmRi+AStlE3PxA=="
+    access_key = "access key here"
     
   }
+
 }
